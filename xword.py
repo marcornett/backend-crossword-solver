16d93e069e87465d1cc026bee985c92b1e2eedcc
Marcel Cornett <marcornett@gmail.com>
2020-09-11 12:47:10 -0400

    Completed crossword solver.

--- xword.py
+++ xword.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import re
 """Crossword Solver Program"""
 
-__author__ = "???"
+__author__ = "marcornett"
 
 # YOUR HELPER FUNCTION GOES HERE
+
+
+def search(reg_str, words):
+    pattern = re.compile(f'{reg_str}')
+    matches = []
+    for word in words:
+        match = pattern.findall(word)
+        if match:
+            matches.extend(match)
+    return ', '.join(matches)
 
 
 def main():
     with open('dictionary.txt') as f:
         words = f.read().split()
-
-    test_word = raw_input(
+    test_word = input(
         'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
-
-    # YOUR ADDITIONAL CODE HERE
-    raise NotImplementedError('Please complete this')
+    reg_str = test_word.replace(' ', "\w")
+    matches = search(reg_str, words)
+    print(matches)
 
 
 if __name__ == '__main__':
